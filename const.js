51658c5af49a50eab47fe9e7cba320d254ca8ca7
Festroud <Roych1993@yandex.ru>
2024-10-04 22:28:11 +0300

    fix const.js

--- const.js
+++ const.js
@@ -183,3 +183,5 @@ module.exports.text82 = text82
 module.exports.text83 = text83
 module.exports.text84 = text84
 module.exports.text85 = text85
+module.exports.text85 = text86
+module.exports.text85 = text87
